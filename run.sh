d6f43f559e450bd1ff5b7e4269e762812a84ffeb
Feodor Rusanov <f.rusanov@gmail.com>
2024-10-10 02:35:06 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,3 +1,34 @@
-export ANSIBLE_LIBRARY=$(pwd)/library;
+sudo pacman -Sy \
+    git \
+    base-devel \
+    archlinux-keyring \
+    --noconfirm;
 
-ansible-playbook -K ./playbooks/*
+# Clone the repository
+git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin;
+cd /tmp/yay-bin;
+makepkg -si;
+
+
+# Install fonts
+yay -S \
+    adobe-source-code-pro-fonts \
+    noto-fonts-emoji \
+    otf-font-awesome \
+    ttf-droid \
+    ttf-fira-code \
+    ttf-jetbrains-mono \
+    ttf-jetbrains-mono-nerd \
+    --noconfirm;
+
+# Hyprland related
+yay -S \
+    hyprcursor \
+    hyprutils \
+    aquamarine \
+    hypridle \
+    hyprlock \
+    hyprland \
+    pyprland \
+    kitty \
+    --noconfirm;
